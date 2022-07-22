9935ddd3d4369448d3ab07e3f1280fb8663d3a26
Marc Obel <marc.obel@outlook.com>
2022-07-22 09:17:57 +0200

    add player position

--- main.lua
+++ main.lua
@@ -1,6 +1,13 @@
+HexSize = 50
+LineDrawMode = 'line'
+FillDrawMode = 'fill'
+Player = {
+  q = 0,
+  r = 0
+}
+
 function love.load()
   InitStyle()
-  HexSize = 50
   love.window.setMode(800, 800)
 end
 
@@ -15,21 +22,21 @@ function InitStyle()
   love.window.setTitle("Hex")
 end
 
-function DrawHex(q, r)
-  local x, y = HexToPixel(q, r)
+function DrawHex(hex, hexSize, fillMode)
+  local x, y = HexToPixel(hex)
   local vertices = {}
-  table.insert(vertices, x + HexSize)
+  table.insert(vertices, x + hexSize)
   table.insert(vertices, y)
   for i = 1, 5 do
-    table.insert(vertices, x + HexSize * math.cos(i * math.pi / 3))
-    table.insert(vertices, y + HexSize * math.sin(i * math.pi / 3))
+    table.insert(vertices, x + hexSize * math.cos(i * math.pi / 3))
+    table.insert(vertices, y + hexSize * math.sin(i * math.pi / 3))
   end
-  love.graphics.polygon("line", vertices)
+  love.graphics.polygon(fillMode, vertices)
 end
 
-function HexToPixel(q, r)
-  local x = HexSize * (3 / 2 * q) + love.graphics.getWidth() / 2
-  local y = HexSize * (math.sqrt(3) / 2 * q + math.sqrt(3) * r) + love.graphics.getHeight() / 2
+function HexToPixel(hex)
+  local x = HexSize * (3 / 2 * hex.q) + love.graphics.getWidth() / 2
+  local y = HexSize * (math.sqrt(3) / 2 * hex.q + math.sqrt(3) * hex.r) + love.graphics.getHeight() / 2
   return x, y
 end
 
@@ -38,7 +45,18 @@ function CreateGrid(mapRadius)
     local r1 = math.max(-mapRadius, -q - mapRadius);
     local r2 = math.min(mapRadius, -q + mapRadius);
     for r = r1, r2 do
-      DrawHex(q, r)
+      local hex = { q = q, r = r }
+      DrawHex(hex, HexSize, LineDrawMode)
+      if IsPlayerPos(Player, hex) then
+        DrawHex(hex, HexSize - 15, FillDrawMode)
+      end
     end
   end
+end
+
+function IsPlayerPos(player, hex)
+  if hex.q == player.q and hex.r == player.r then
+    return true
+  end
+  return false
 end
