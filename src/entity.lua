e19487b7533a399b530bf5121598b8dbb1e3672f
Lucas Poulsen <lucasmbpoulsen@hotmail.com>
2022-08-06 10:49:43 +0200

    Add color palette to the player

--- src/entity.lua
+++ src/entity.lua
@@ -1,4 +1,8 @@
 function DrawPlayerHex(playerHex)
+    local prevCol = { love.graphics.getColor() }
+    love.graphics.setColor(Palette.Player)
     DrawHex(playerHex, Settings.HexSize, 'line')
     DrawHex(playerHex, Settings.HexSize - 15, 'fill')
+    love.graphics.setColor(prevCol)
 end
+
